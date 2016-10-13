62371a7cce10d53a8848e82f1bb954dc6e97d8a3
Christoph Gockel <github@christophgockel.de>
2016-10-13 22:43:01 +0100

    Adds a MoviePresenter

---
+++ spec/movie_presenter_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+require "movie_presenter"
+
+RSpec.describe MoviePresenter do
+  it "titleizes the title" do
+    movie = Movie.new(title: "the movie title")
+    presenter = described_class.new(movie)
+
+    expect(presenter.title).to eq("The Movie Title")
+  end
+
+  it "formats the release date" do
+    movie = Movie.new(release_date: Date.parse("1984-06-08"))
+    presenter = described_class.new(movie)
+
+    expect(presenter.release_date).to eq("8 June 1984")
+  end
+
+  it "presents the description" do
+    movie = Movie.new(description: "the description")
+    presenter = described_class.new(movie)
+
+    expect(presenter.description).to eq(movie.description)
+  end
+end
